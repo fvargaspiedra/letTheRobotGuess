62618d2a3a3630644023a88e6d7c9a7441a677c0
Andrés Díaz Soto <andres@estudiomanati.com>
2018-04-26 16:14:32 -0600

    Frontend game logic and UI improvements

--- client/src/components/VideoCapture.vue
+++ client/src/components/VideoCapture.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="video-capture">
-    <div class="video-container">
-      <div><video ref="video" id="video" width="640" height="480" autoplay></video></div>
-      <div class="crop-square"></div>
+    <div class="controls">
+      <button @click="play()" v-if="!gameStarted">Let's Play!</button>
+      <button id="snap" @click="capture()" v-if="gameStarted">Take Picture</button>
     </div>
-    <div><button id="snap" @click="capture()">Snap Photo</button></div>
+    <div class="game">
+      <div class="video-container">
+        <div><video ref="video" id="video" width="640" height="480" autoplay></video></div>
+        <div class="crop-square"></div>
+      </div>
+      <div class="messages">
+        <p v-if="selectedCategory">Draw: {{ selectedCategory }}</p>
+        <p v-if="guessed">The robot's guess: {{ guessed }}
+          <span v-if="gameCompleted">- YOU WIN!</span>
+          <span v-if="!gameCompleted">- Try again</span>
+        </p>
+      </div>
+    </div>
     <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
 
 </div>
@@ -18,7 +30,14 @@ export default {
   data () {
     return {
       video: {},
-      canvas: {}
+      canvas: {},
+      categories: [],
+      gameStarted: false,
+      gameCompleted: false,
+      gameWon: false,
+      guessed: '',
+      selectedCategory: ''
+
     }
   },
   mounted () {
@@ -32,25 +51,64 @@ export default {
   },
   methods: {
     capture () {
+      this.guessed = ''
       this.canvas = this.$refs.canvas
       this.canvas.getContext('2d').drawImage(this.video, 0, 0, 640, 480)
       const image = this.canvas.toDataURL('image/png')
-      console.log(image)
       axios.post('http://localhost:5000/classify', {
         image: image
       }).then((response) => {
-        console.log(response)
+        this.guessed = response.data.guess
+        if (response.data.guess === this.selectedCategory) {
+          this.gameCompleted = true
+          this.gameStarted = false
+        }
       }).catch((error) => {
         console.log(error)
       })
+    },
+    play() {
+      axios.get('http://localhost:5000/getLabels').then((response) => {
+        console.log(response)
+        this.categories = response.data
+        this.gameStarted = true
+        this.gameCompleted = false
+        this.selectedCategory = this.categories[Math.floor(Math.random() * this.categories.length)]
+        console.log(this.selectedCategory)
+      });
     }
   }
 }
 </script>
 
 <style lang="scss">
+.controls {
+  margin-bottom: 50px;
+  text-align: center;
+}
+button {
+  padding: 10px 30px;
+  color: #fff;
+  background-color: #4DA1CD;
+  border-radius: 3px;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  &:hover, &:active {
+    background-color: darken(#4DA1CD, 10%);
+  }
+}
+.game {
+  display: flex;
+}
 .video-container {
   position: relative;
+  flex: 0 0 640px;
+}
+.messages {
+  flex: 1 1 auto;
+  margin-left: 30px;
+  font-size: 20px;
 }
 .crop-square {
   position: absolute;
@@ -60,5 +118,8 @@ export default {
   top: calc(50% - 150px);
   z-index: 10;
   border: red solid 3px;
+}
+#canvas {
+  display: none;
 }
 </style>
